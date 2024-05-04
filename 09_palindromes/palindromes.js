3e48832a00eb5d1c0211fc2fc98df2da9de09c40
iamcarlosamaya <cje.amaya@gmail.com>
2024-05-04 11:51:15 -0400

    Added palindrome function and validated the tests

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,22 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    let rWord = ''; // Reverse word initializer
+    const regex = /[\W]/g; // Set regex criteria
+    
+    // clean the word before initiating the palindrome test
+    let cleanedWord = string.replace(regex, '');
+    cleanedWord = cleanedWord.toLowerCase();
+    console.log(cleanedWord);
 
+    // Test if palindrome
+    for (let i = cleanedWord.length - 1; i >= 0; i--) {
+        // console.log(string[i])
+        rWord += cleanedWord[i];
+    }
+    return cleanedWord === rWord;
+    // console.log(newWord);
 };
+
+// console.log(palindromes('hola'));
 
 // Do not edit below this line
 module.exports = palindromes;
